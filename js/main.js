5ee57351e8b7017d408007f1e22189d5fea6e415
Adelto Oliveira <contato.adtodeveloper@gmail.com>
2022-05-07 23:24:29 -0300

    Finalizando o menu mobile, fazendo interacao com js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+const btnOpen = document.querySelector('#btn-open')
+const btnClosed = document.querySelector('#btn-closed')
+const body = document.querySelector('body')
+
+
+//SCROLL NAV TOGGLE COLOR
 const onScroll = () => { 
     (scrollY > 0)   
     ? navigation.classList.add('scroll') 
     : navigation.classList.remove('scroll')
 }
+
+//MENU TOGGLE 
+const onClick = () => {
+    btnOpen.addEventListener('click', () => 
+        body.classList.add('menu-mobile-extended'))
+        
+    btnClosed.addEventListener('click', () => 
+        body.classList.remove('menu-mobile-extended'))
+}
+
+
+   
